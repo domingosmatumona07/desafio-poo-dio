bd1a34bf1d371c5a82d1a7e32be86b25b14a9eb3
Domingos Matumona <domingosmatumona73@gmail.com>
2022-02-20 06:28:01 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
+import org.w3c.dom.ls.LSOutput;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 
 public class Main {
@@ -15,13 +16,49 @@ public class Main {
         curso2.setDescricao("Descrição Curso JavaScript");
         curso2.setCargaHoraria(4);
 
+        Conteudo conteudo = new Curso();
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMatumona = new Dev();
+        devMatumona.setNome("Matumona");
+        devMatumona.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Matumona " + devMatumona.getConteudosInscritos());
+
+        devMatumona.progredir();
+        devMatumona.progredir();
+        System.out.println("--");
+        System.out.println("Conteúdos inscritos Matumona " + devMatumona.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Matumona " + devMatumona.getConteudosConcluidos());
+        System.out.println("XP: " + devMatumona.calcularXp());
+
+        System.out.println("-----------------");
+
+        Dev devSuzana = new Dev();
+        devSuzana.setNome("Suzana");
+        devSuzana.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Suzana " + devSuzana.getConteudosInscritos());
+
+        devSuzana.progredir();
+        devSuzana.progredir();
+        devSuzana.progredir();
+        System.out.println("--");
+        System.out.println("Conteúdos inscritos Suzana " + devSuzana.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Suzana " + devSuzana.getConteudosConcluidos());
+        System.out.println("XP: " + devSuzana.calcularXp());
     }
 }
